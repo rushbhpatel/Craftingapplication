146af0e08d719171d315bc771ab151235564947e
rush <rp3048@yahoo.com>
2017-11-08 04:23:05 +0000

    lab 08

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,11 +26,10 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    product = Product.find(params[:product_id])
-	@line_item = @cart.line_items.build(product: product)
-   # @line_item = LineItem.new(line_item_params)
-
-    respond_to do |format|
+     product = Product.find(params[:product_id])
+     @line_item = @cart.add_product(product)
+ 
+     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         #format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
@@ -72,8 +71,9 @@ class LineItemsController < ApplicationController
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+  # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+    #  params.require(:line_item).permit(:product_id, :cart_id)
+    params.require(:line_item).permit(:product_id)
     end
 end
